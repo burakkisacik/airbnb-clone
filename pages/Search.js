c10fdca70e8e797260a449dd6515ab059250fa9b
Burak Kısacık <burakkisacik@Burak-MacBook-Air.local>
2021-08-23 15:56:28 +0300

    Add search page

---
+++ pages/Search.js
@@ -0,0 +1,79 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { useRouter } from 'next/dist/client/router';
+import { format } from 'date-fns';
+import InfoCard from '../components/InfoCard';
+
+function Search({ searchResults }) {
+    const router = useRouter();
+
+    const { location, startDate, endDate, noOfGuests } = router.query;
+
+    const formattedStartDate = format(new Date(startDate), 'dd MMMM yyy');
+
+    const formattedEndDate = format(new Date(endDate), 'dd MMMM yyy');
+
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+    const placeholder = `${location} | ${range} | ${noOfGuests} guests`;
+
+    return (
+        <div>
+            <Header placeholder={placeholder} />
+
+            <main className='flex'>
+                <section className='flex-grow pt-14 px-6'>
+                    <p className='text-xs'>
+                        300+ Stays {range} for {noOfGuests} guests
+                    </p>
+                    <h1 className='text-3xl font-semibold mt-2 mb-6'>
+                        Stays in {location}
+                    </h1>
+
+                    <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+                        <p className='button'>Cancellation Flexibility</p>
+
+                        <p className='button'>Type of Place</p>
+
+                        <p className='button'>Price</p>
+
+                        <p className='button'>Rooms and Beds</p>
+
+                        <p className='button'>More Filters</p>
+                    </div>
+
+                    <div className='flex flex-col'>
+                        {searchResults?.map((item) => (
+                            <InfoCard
+                                key={item.img}
+                                img={item.img}
+                                title={item.title}
+                                description={item.description}
+                                price={item.price}
+                                location={item.location}
+                                star={item.star}
+                                total={item.total}
+                            />
+                        ))}
+                    </div>
+                </section>
+            </main>
+
+            <Footer />
+        </div>
+    );
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+    const searchResults = await fetch('https://links.papareact.com/isz').then(
+        (response) => response.json()
+    );
+
+    return {
+        props: {
+            searchResults: searchResults,
+        },
+    };
+}
