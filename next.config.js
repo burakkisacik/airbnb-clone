6e977809fa9ab6424ae05951320fb6e45bc46bc5
Burak Kısacık <burakkisacik@Burak-MacBook-Air.local>
2021-08-23 17:49:37 +0300

    Add mapbox

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     images: {
         domains: ['links.papareact.com'],
     },
+    env: {
+        mapbox_key:
+            'pk.eyJ1IjoiYnVyYWtiZXkwOCIsImEiOiJja25mdzk4ZjkyanBoMnF0YTYwMXVmd3hqIn0.bSATM5hIKxeJHytjc1Kc1A',
+    },
 };
